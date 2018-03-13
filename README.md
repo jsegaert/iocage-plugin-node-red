f8cb76d71bbbb143634368c6a6cd604ddf41832c
Jurgen Segaert <jsegaert@yahoo.com>
2018-03-12 22:11:19 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # iocage-plugin-node-red
 Artifact file(s) for Node-RED iocage plugin
+
+## To install this plugin
+Download the plugin manifest file to your local file system.
+```
+fetch https://raw.githubusercontent.com/jsegaert/iocage-my-plugins/master/node-red.json
+```
+Install the plugin.  Adjust host interface and IP address as needed.  
+```
+iocage fetch -P -n node-red.json ip4_addr="em0|192.168.0.100"
+```
+
---
+++ post_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Defaults for Node-RED settings
+user_name=nrserver
+user_uid=2001
+
+# Node-RED post install
+mkdir -p /usr/local/etc/rc.d
+ln -s /usr/local/bin/bash /bin/bash
+
+npm install -g --unsafe-perm node-red
+
+# Create Node-RED server
+pw useradd -n $user_name -u $user_uid -s /usr/local/bin/bash -w random -m
+
+# Enable the service
+chmod +x /usr/local/etc/rc.d/nodered
+sysrc -f /etc/rc.conf nodered_enable="YES"
+
+# Start the service
+service nodered start 2>/dev/null
+
